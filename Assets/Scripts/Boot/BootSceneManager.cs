ec934cfdf781f8ad9c2af844f9152c07c9de72a5
Marumaro2555 <marumaro2555@gmail.com>
2019-07-21 13:38:38 +0900

    スクリプト追加

---
+++ Assets/Scripts/Boot/BootSceneManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class BootSceneManager : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Title/TitleSceneManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    [SerializeField] private Button _startButton = null;
+
+    private void Start()
+    {
+        _startButton.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                SceneManager.LoadSceneAsync("MenuScene");
+            })
+            .AddTo(this);
+    }
+}
