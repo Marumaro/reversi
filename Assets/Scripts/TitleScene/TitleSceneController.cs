c4eed428fef67d8941ac7ddc5df785f647e8f791
Marumaro2555 <marumaro2555@gmail.com>
2019-08-04 02:44:59 +0900

    Title改修

---
+++ Assets/Scripts/TitleScene/TitleSceneController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace Reversi.Scene
+{
+    public class TitleSceneController : ControllerBase
+    {
+        [SerializeField]
+        private Button _startButton = null;
+
+        public override void Initialize()
+        {
+            _startButton.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    SceneTransitionManager.Instance.Transition(SceneBuildIndex.MenuScene, null);
+                })
+                .AddTo(this);
+        }
+    }
+}
---
+++ Assets/Scripts/TitleScene/TitleSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Reversi.Scene
+{
+    public class TitleSceneManager : SceneManagerBase
+    {
+        [SerializeField]
+        private TitleSceneController _titleSceneController = null;
+
+        public override void Initialize(SceneArgBase argBase)
+        {
+            var arg = ConvertArg<TitleSceneArg>(argBase);
+            Debug.Log(arg.TestString);
+
+            _titleSceneController.Initialize();
+        }
+    }
+
+    public class TitleSceneArg : SceneArgBase
+    {
+        public readonly string TestString;
+
+        public TitleSceneArg(string testString)
+        {
+            TestString = testString;
+        }
+    }
+}
