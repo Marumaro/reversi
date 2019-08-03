0fd33822877403502e0e0f6a5131d0ba8a92a09a
Marumaro2555 <marumaro2555@gmail.com>
2019-08-04 02:45:26 +0900

    Menu改修

---
+++ Assets/Scripts/MenuScene/MenuSceneController.cs
@@ -0,0 +1,34 @@
+﻿using Reversi.Dialog;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Reversi.Scene
+{
+    public class MenuSceneController : ControllerBase
+    {
+        [SerializeField]
+        private Button _singleButton = null;
+
+        [SerializeField]
+        private Button _multiButton = null;
+
+        public override void Initialize()
+        {
+            _singleButton.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+
+                })
+                .AddTo(this);
+
+            _multiButton.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    var arg = new MultiPlayDialogArg("マルチプレイ", null);
+                    var dialog = DialogManager.Instance.Open<MultiPlayDialog>(arg);
+                })
+                .AddTo(this);
+        }
+    }
+}
---
+++ Assets/Scripts/MenuScene/MenuSceneManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Reversi.Scene
+{
+    public class MenuSceneManager : SceneManagerBase
+    {
+        [SerializeField]
+        private MenuSceneController _menuSceneController = null;
+
+        public override void Initialize(SceneArgBase argBase)
+        {
+            _menuSceneController.Initialize();
+        }
+    }
+}
