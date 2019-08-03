40aaeb863ce14011a2159e0b49ef1a1b3b5fdcc7
Marumaro2555 <marumaro2555@gmail.com>
2019-08-04 02:37:21 +0900

    シーンマネージャーのベースクラス追加

---
+++ Assets/Scripts/SceneManagerBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Reversi.Scene
+{
+    public abstract class SceneManagerBase : MonoBehaviour
+    {
+        public abstract void Initialize(SceneArgBase argBase);
+
+        protected T ConvertArg<T>(SceneArgBase argBase) where T : SceneArgBase
+        {
+            var arg = argBase as T;
+            if (arg == null)
+            {
+                Debug.LogErrorFormat("{0} is different arg", arg);
+            }
+
+            return arg;
+        }
+    }
+
+    public abstract class SceneArgBase
+    {
+    }
+}
