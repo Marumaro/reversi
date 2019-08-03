0b022c448f3814c42f9d1d4547f661f3a5a7167e
Marumaro2555 <marumaro2555@gmail.com>
2019-08-04 02:36:01 +0900

    ビルドインデックスクラス生成

---
+++ Assets/SceneBuildIndex.cs
@@ -0,0 +1,10 @@
+﻿/// <summary>
+/// シーンのビルドインデックスを定数で管理するクラス
+/// </summary>
+public class SceneBuildIndex
+{
+	public const int BootScene = 0;
+	public const int TitleScene = 1;
+	public const int MenuScene = 2;
+	public const int RoomScene = 3;
+}
