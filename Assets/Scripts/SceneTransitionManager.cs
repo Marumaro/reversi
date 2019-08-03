5ab8dca64de22b3a7748432a9267141f39bd57f9
Marumaro2555 <marumaro2555@gmail.com>
2019-08-04 02:39:48 +0900

    シーン遷移クラス追加

---
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Reversi.Scene
+{
+    public class SceneTransitionManager : SingletonMonoBehaviour<SceneTransitionManager>
+    {
+        private SceneManagerBase _currentSceneManager;
+
+        public void Transition(int sceneBuildIndex, SceneArgBase argBase)
+        {
+            StartCoroutine(SceneLoadCoroutine(sceneBuildIndex, argBase));
+        }
+
+        public IEnumerator SceneLoadCoroutine(int sceneBuildIndex, SceneArgBase argBase)
+        {
+            yield return SceneManager.LoadSceneAsync(sceneBuildIndex);
+
+            _currentSceneManager = GameObject.Find("SceneManager").GetComponent<SceneManagerBase>();
+            if (_currentSceneManager == null)
+            {
+                Debug.LogError("SceneManager not found");
+                yield break;
+            }
+
+            _currentSceneManager.Initialize(argBase);
+        }
+    }
+}
