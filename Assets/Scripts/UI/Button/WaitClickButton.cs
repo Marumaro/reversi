0acdd531398a6da2f52997eeed25ca00f305809d
Marumaro2555 <marumaro2555@gmail.com>
2019-08-04 02:34:59 +0900

    待機ボタン追加

---
+++ Assets/Scripts/UI/Button/WaitClickButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent, RequireComponent(typeof(Image)), RequireComponent(typeof(Button))]
+public class WaitClickButton : MonoBehaviour
+{
+    /// <summary> ボタン </summary>
+    [SerializeField]
+    private Button _button = null;
+
+    /// <summary> クリックされたか </summary>
+    private bool _isClicked;
+
+    /// <summary>
+    /// Start
+    /// </summary>
+    private void Start()
+    {
+        // ボタンの処理を購読する
+        _button
+           .OnClickAsObservable()
+           .Subscribe(_ => { _isClicked = true; });
+    }
+
+    /// <summary>
+    /// クリック待ち
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator WaitClick()
+    {
+        _isClicked = false;
+        yield return new WaitUntil(() => _isClicked);
+    }
+}
+
